7206c1712f2baa77a884a0e91ce3f7562267507f
wangdan <dan.wang@fit2cloud.com>
2023-08-10 15:40:49 +0800

    feat:

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { defineAsyncComponent } from 'vue'
-import { type RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
+import { defineAsyncComponent } from 'vue';
+import { type RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
 
-export const Layout = () => import('@/layout/index.vue')
-const modules: any = import.meta.glob('./modules/*.ts')
+export const Layout = () => import('@/layout/index.vue');
+const modules: any = import.meta.glob('./modules/*.ts');
 
 /** 常驻路由 */
 export const constantRoutes: RouteRecordRaw[] = [
@@ -18,26 +18,26 @@ export const constantRoutes: RouteRecordRaw[] = [
         meta: {
           title: '首页',
           icon: 'HomeFilled',
-          affix: true
-        }
-      }
-    ]
+          affix: true,
+        },
+      },
+    ],
   },
 
   {
     path: '/403',
     component: () => import('@/views/error-page/403.vue'),
     meta: {
-      hidden: true
-    }
+      hidden: true,
+    },
   },
   {
     path: '/404',
     component: () => import('@/views/error-page/404.vue'),
     meta: {
-      hidden: true
+      hidden: true,
     },
-    alias: '/:pathMatch(.*)*'
+    alias: '/:pathMatch(.*)*',
   },
   {
     path: '/router-demo',
@@ -45,7 +45,7 @@ export const constantRoutes: RouteRecordRaw[] = [
     name: 'RouterDemo',
     meta: {
       title: '路由示例',
-      icon: 'Share'
+      icon: 'Share',
     },
     children: [
       {
@@ -53,16 +53,16 @@ export const constantRoutes: RouteRecordRaw[] = [
         component: () => import('@/views/router-demo/RouterDocs.vue'),
         name: 'RouterDocs',
         meta: {
-          title: '官方文档'
-        }
+          title: '官方文档',
+        },
       },
       {
         path: 'menu',
         component: () => import('@/views/router-demo/RouterMenu.vue'),
         name: 'RouterMenu',
         meta: {
-          title: '路由与菜单'
-        }
+          title: '路由与菜单',
+        },
       },
       {
         path: 'detail',
@@ -70,16 +70,16 @@ export const constantRoutes: RouteRecordRaw[] = [
         name: 'RouterDetail',
         meta: {
           activeMenu: '/router-demo/menu',
-          hidden: true
-        }
+          hidden: true,
+        },
       },
       {
         path: 'keep-alive',
         component: () => import('@/views/router-demo/RouterKeepAlive.vue'),
         name: 'RouterKeepAlive',
         meta: {
-          title: '组件缓存'
-        }
+          title: '组件缓存',
+        },
       },
       {
         path: 'keep-alive-detail',
@@ -88,10 +88,10 @@ export const constantRoutes: RouteRecordRaw[] = [
         meta: {
           cache: true,
           activeMenu: '/router-demo/menu',
-          hidden: true
-        }
-      }
-    ]
+          hidden: true,
+        },
+      },
+    ],
   },
 
   {
@@ -100,44 +100,52 @@ export const constantRoutes: RouteRecordRaw[] = [
     name: 'Components',
     meta: {
       title: '组件',
-      icon: 'Grid'
+      icon: 'Grid',
     },
     children: [
+      {
+        path: 'tabs-demo',
+        component: () => import('@/views/components/DETabsDemo.vue'),
+        name: 'DETabsDemo',
+        meta: {
+          title: 'DE 标签页',
+        },
+      },
       {
         path: 'layout-content',
         component: () => import('@/views/components/LayoutContentDemo.vue'),
         name: 'LayoutContentDemo',
         meta: {
-          title: '页面布局'
-        }
+          title: '页面布局',
+        },
       },
       {
         path: 'card-view',
         component: () => import('@/views/components/ViewCardDemo.vue'),
         name: 'ViewCardDemo',
         meta: {
-          title: '查看Card'
-        }
-      }
-    ]
-  }
-]
+          title: '查看Card',
+        },
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: constantRoutes as RouteRecordRaw[],
   // 刷新时，滚动条位置还原
-  scrollBehavior: () => ({ left: 0, top: 0 })
-})
+  scrollBehavior: () => ({ left: 0, top: 0 }),
+});
 
 // 重置路由
 export function resetRouter() {
   router.getRoutes().forEach((route) => {
-    const { name } = route
+    const { name } = route;
     if (name && router.hasRoute(name)) {
-      router.removeRoute(name)
+      router.removeRoute(name);
     }
-  })
+  });
 }
 
-export default router
+export default router;
---
+++ src/views/components/DETabsDemo.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="tabs-demo">
+    <h3>无配置状态</h3>
+    <dataease-tabs v-model="activeName" type="card">
+      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+    </dataease-tabs>
+    <h3 style="margin-top: 50px">无边框</h3>
+    <dataease-tabs
+      v-model="activeName"
+      :font-color="fontColor"
+      :active-color="activeColor"
+      :border-color="noBorderColor"
+      :border-active-color="borderActiveColor"
+      type="card">
+      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+    </dataease-tabs>
+    <h3 style="margin-top: 50px">选项卡样式</h3>
+    <dataease-tabs
+      v-model="activeName"
+      :font-color="fontColor"
+      :active-color="activeColor"
+      :border-color="borderColor"
+      :border-active-color="borderActiveColor"
+      type="card">
+      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+    </dataease-tabs>
+    <h3 style="margin-top: 50px">简洁的标签页</h3>
+    <dataease-tabs v-model="activeName" :font-color="fontColor2" :active-color="activeColor2" :border-color="borderColor2">
+      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+    </dataease-tabs>
+
+    <h3 style="margin-top: 50px">radioGroup类型标签页</h3>
+    <dataease-tabs
+      v-model="activeName"
+      type="card"
+      style-type="radioGroup"
+      :font-color="fontColor"
+      :active-color="activeColor3"
+      :border-color="borderColor3"
+      :border-active-color="borderActiveColor3">
+      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+    </dataease-tabs>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import DataeaseTabs from '@/components/dataease-tabs/index.vue';
+
+const activeName = ref('first');
+const fontColor = ref('#999999');
+const activeColor = ref('#f18406');
+const borderColor = ref('#999999'); // 可以设 none 全部边框消失
+const borderActiveColor = ref('#f18406');
+// 简洁的标签页
+const fontColor2 = ref('#333333');
+const activeColor2 = ref('blue');
+const borderColor2 = ref('#999999');
+
+// 特殊的标签页 radioGroup类型
+const borderColor3 = ref('#999999');
+const activeColor3 = ref('#f18406');
+const borderActiveColor3 = ref('#f18406');
+
+// 无边框
+const noBorderColor = ref('none');
+</script>
